60d254808cb082e9335a31edacfbb3190120ee2b
Jaune Sarmiento <hawnecarlo@gmail.com>
2013-05-06 14:27:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Fries
+
+Fries is an awesome mobile prototyping framework for Android apps inspired by [Ratchet]. 
+We all know that you can find loads of iOS prototyping tools out there, so this time 
+let's give some love to Android.
+
+
+## Getting Started
+
+Read the [docs] to see it in action.
+
+
+## Browser Compatibility
+
+Coming soon as Fries is currently being deep fried in most modern mobile browsers.
+
+
+## Other Notes
+
+Fries uses [Push.js], also from Ratchet, to piece together all pages for the prototype.
+You might want to read about it before using Fries.
+
+
+## Special Thanks
+
+I'd like to thank [Dave Gamache], [Connor Sears], and [Jacob Thornton] for a wonderful job on [Ratchet].
+Also, Fries' scroll bars are made using [iScroll 4] by [Matteo Spinelli]. Kudos to you guys.
+
+
+## License
+
+__The MIT License (MIT)__
+
+Copyright (c) 2013 [Jaune Sarmiento]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+[Ratchet]: http://maker.github.io/ratchet
+[Push.js]: http://maker.github.io/ratchet/#push
+[Jaune Sarmiento]: http://jaunesarmiento.me
+[docs]: http://jaunesarmiento.me/fries
+[Dave Gamache]: http://github.com/dhgamache
+[Connor Sears]: http://github.com/connors
+[Jacob Thornton]: http://github.com/fat
+[iScroll 4]: http://cubiq.org/iscroll-4
+[Matteo Spinelli]: http://cubiq.org/
---
+++ docs/js/docs.js
@@ -0,0 +1,145 @@
+$(function() {
+
+  var scroller;
+  var $win;
+  var $doc;
+  var $bod;
+  var galaxy;
+  var contentSection;
+  var topCache;
+  var windowWidth;
+  var eventListeners;
+  var pageHeight;
+  var windowHeight;
+  var footerHeight;
+  var currentActive;
+  var $toggleSpinner;
+  var $navComponents;
+
+  prettyPrint();
+
+  var initialize = function  () {
+    attachIScroll();
+
+    $win = $(window);
+    $doc = $(document);
+    $bod = $(document.body);
+
+    $toggleSpinner = $('.toggle-spinner');
+    $navComponents = $('.nav-components');
+    
+    galaxy = galaxy || $('.galaxy');
+    contentSection = $('.component');
+    topCache = contentSection.map(function () { return $(this).offset().top });
+    contentPadding = 0;
+
+    pageHeight = $doc.height();
+    windowHeight = $win.height() / 3;
+    footerHeight = 0;
+
+    checkDesktopContent();
+    calculateScroll();
+
+    if (!eventListeners) addEventListeners();
+  }
+
+  var attachIScroll = function() {
+    // If there is nothing to scroll, don't worry about this
+    if ($('#scroll').length == 0) return;
+
+    // Destroy it first
+    if (scroller) {
+      scroller.destroy();
+      scroller = null;
+    }
+
+    setTimeout(function(){
+        scroller = new iScroll('scroll', {
+          fadeScrollbar: true,
+          hideScrollbar: true
+        });
+    }, 0);
+  }
+
+  var addEventListeners = function () {
+    eventListeners = true;
+    
+    galaxy.on('click', function (e) {
+      e.preventDefault();
+    });
+
+    $toggleSpinner.click(function(e) {
+      e.stopPropagation();
+      e.preventDefault();
+      $navComponents.toggleClass('active');
+    })
+
+    $doc.on('click', function () {
+      $navComponents.removeClass('active');
+    })
+
+    // Hacky fix for parts of the header magically disappearing
+    $('.nav-item a').on('click', function (e) {
+      window.scrollTo(window.pageXOffset, window.pageYOffset - 1);
+    });
+
+    $(window).on('scroll', calculateScroll);
+    $(window).on('scroll', attachIScroll);
+  }
+
+  var checkDesktopContent = function () {
+    windowWidth = $(window).width();
+    if (windowWidth <= 768) {
+      var content = $('.content')
+      if (content.length > 1) {
+        $(content[0]).remove()
+      }
+    }
+  }
+
+  var calculateScroll = function () {
+    // if small screen don't worry about this
+    if (windowWidth <= 768) return
+
+    // Save scrollTop value
+    var contentSectionItem;
+    var currentTop = $win.scrollTop();
+
+    // If page is scrolled to bottom near footers
+    /*
+    if(pageHeight - currentTop < footerHeight + contentPadding + 1400) {
+      galaxy[0].className = "galaxy galaxy-bottom";
+      galaxy[0].setAttribute('style','')
+    } else {
+      galaxy[0].className = "galaxy galaxy-fixed"
+      galaxy[0].setAttribute('style','')
+    }
+    */
+
+    // Injection of components into phone
+    for (var l = contentSection.length; l--;) {
+      if ((topCache[l] - currentTop) < windowHeight) {
+        if (currentActive == l) return;
+        currentActive = l;
+        $bod.find('.component.active').removeClass('active');
+        contentSectionItem = $(contentSection[l])
+        contentSectionItem.addClass('active')
+        if(contentSectionItem.attr('id')) {
+          galaxy.attr("id", contentSectionItem.attr('id') + "InPhone");
+        } else {
+          galaxy.attr("id", "")
+        }
+        if (!contentSectionItem.hasClass('informational')) {
+          updateContent(contentSectionItem.find('.prettyprint').not('.js').text())
+        }
+        break
+      }
+    }
+  }
+
+  var updateContent = function (content) {
+    $('#galaxy-window').html(content);
+  }
+
+  $(window).on('load resize', initialize);
+});
