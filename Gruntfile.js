ca657e307af6b4e49e1bf906f3a955e89c0f026a
Jaune Sarmiento <hawnecarlo@gmail.com>
2013-09-30 01:29:34 +0800

    Adds missing semicolon

--- Gruntfile.js
+++ Gruntfile.js
@@ -101,7 +101,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'sass', 'cssmin', 'copy']);
 
