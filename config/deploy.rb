520cab7429a85368e8bb80e62478f3404147a1a3
Nico Bantatua <nbantatua@gmail.com>
2014-06-04 19:53:04 +0800

    Deploy.rb cleanup

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,13 +6,11 @@ set :repository, 'git@github.com:jaunesarmiento/fries.git'
 set :scm, :git
 set :scm_verbose, true
 set :deploy_via, :remote_cache
-# set :copy_compression, :gzip
-# set :use_sudo, false
 
 set :user, "deploy"
 set :password, "grabekadud3"
 set :deploy_to, "/data/#{application}"
-set :branch, "capistrano"
+set :branch, "capistrano" # change this to "develop" after PR merge
 
 set :default_run_options, { pty: true }
 set :ssh_options, { forward_agent: true }
