5c5725334ae81ede085ea2227600a672e4b3c61a
Nico Bantatua <nbantatua@gmail.com>
2014-06-04 19:51:53 +0800

    Deploy.rb cleanup

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,23 +19,6 @@ set :ssh_options, { forward_agent: true }
 
 role :web, "106.186.25.187"
 
-# before 'deploy:update', 'deploy:update_jekyll'
-
-namespace :deploy do
-  [:start, :stop, :restart, :finalize_update].each do |t|
-    desc "#{t} task is a no-op with jekyll"
-    task t, :roles => :app do ; end
-  end
-
-  desc 'Run jekyll to update site before uploading'
-  task :update_jekyll do
-    # clear existing _site
-    # build site using jekyll
-    # remove Capistrano stuff from build
-    %x(rm -rf _site/* && jekyll build && rm _site/Capfile && rm -rf _site/config)
-  end
-end
-
 after "deploy:create_symlink" do
   run "rm -rf #{release_path}/config #{release_path}/Capfile"
   run "cd #{release_path} && jekyll build"
